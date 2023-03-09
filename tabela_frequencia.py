533fa66847e52b4007c4e5b048e6a7eb09c9b9fc
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2023-03-09 15:33:48 -0300

    Update tabela_frequencia.py

--- tabela_frequencia.py
+++ tabela_frequencia.py
@@ -1,28 +1,45 @@
 ############################## BIBLIOTECAS ############################################
 import numpy as np
 import pandas as pd
+from prettytable import PrettyTable
 
 #############################   TABELA    #############################################
-tabela = pd.read_csv(r"digite o caminho da planilha aqui") # Passe o caminho da planilha dentro das aspas
+x1 = pd.read_csv(r"digite o nome do arquivo .csv") # Passe o caminho da planilha dentro das aspas
 
-col = tabela["digite a coluna aqui"].tolist() # Transforma coluna em lista
+X = x1["digite a coluna aqui"].tolist() # Transforma coluna em lista
 
-observando = list(set(col)) # Remove os valores duplicados / será usado como referência
+# Total
+n = len(X)
 
-k = len(observando) # Contar os elementos que se repetem
+obs = list(set(X)) # Remove os valores duplicados / será usado como referência
+
+k = len(obs) # Contar os elementos que se repetem
 
 ############################# FREQ. ABSOLUTA ##########################################
-freq_abs = [col.count(observando[i]) for i in range(0, k)]
+freq_abs = [X.count(obs[i]) for i in range(0, k)]
 
 ############################# FREQ. RELATIVA ##########################################
-freq_rel = [round(col.count(observando[i]) / k, 4) for i in range(0, k)]
+freq_rel = [round(X.count(obs[i]) / n, 4) for i in range(0, k)]
 
 
 ####################### FREQ. RELATIVA PERCENTUAL #####################################
-freq_rel_per = [round((col.count(observando[i]) / k) * 100, 2) for i in range(0, k)]
+freq_rel_per = [round((X.count(obs[i]) / n) * 100, 2) for i in range(0, k)]
 
 ####################### FREQ. ABSOLUTA AC #############################################
 freq_abs_ac = list(np.cumsum(freq_abs))
 
 ####################### FREQ. RELATIVA PERCENTUAL AC ##################################
 freq_rel_per_ac = list(np.cumsum(freq_rel_per))
+
+
+####################### TABELA DE FREQUÊNCIAS #########################################
+tabela_freqs = PrettyTable()
+tabela_freqs.add_column("Dados", obs)
+tabela_freqs.add_column("FA", freq_abs)
+tabela_freqs.add_column("FR", freq_rel)
+tabela_freqs.add_column("FR%", freq_rel_per)
+tabela_freqs.add_column("FAC", freq_abs_ac)
+tabela_freqs.add_column("FAR%", freq_rel_per_ac)
+
+tabela_freqs.add_row(["Total:", sum(freq_abs), sum(freq_rel), sum(freq_rel_per), "-", "-"])
+print(tabela_freqs)
